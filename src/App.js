219bb4c931bec30661247ee63cb2efae9f9c84b4
kidman90 <kacprzak90@gmail.com>
2019-10-08 15:40:07 +0200

    Dependencies installed, connecting ApolloClient

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
 import logo from './logo.svg';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: 'https://vm8mjvrnv3.lp.gql.zone/graphql'
+});
 
 function App() {
   return (
